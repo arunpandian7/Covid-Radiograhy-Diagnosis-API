bd3323429565b1199cedebfb978c0a6d419a14a2
Arun Pandian R <arunrk7.leo@gmail.com>
2022-05-01 16:58:56 +0000

    Refactor Model Inference Pipeline

---
+++ backend/inference/covid_classification.py
@@ -0,0 +1,63 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+import tensorflow_hub as tfhub
+import numpy as np
+
+from backend import config
+
+MODEL_ARCH_PATH = "models/configs/tf_hub_effv2/"
+MODEL_BASE_PATH = "models/weights/efficient-classifer"
+target_size=(768, 768)
+
+LABELS = ['Negative', 'Typical Appearance', 'Indeterminate', 'Atypical']
+
+devices = tf.config.list_physical_devices()
+tf.config.experimental.set_memory_growth(
+    devices[1], True
+)
+
+# Custom wrapper class to load the right pretrained weights explicitly from the local directory
+class KerasLayerWrapper(tfhub.KerasLayer):
+    def __init__(self, handle, **kwargs):
+        handle = tfhub.KerasLayer(tfhub.load(MODEL_ARCH_PATH))
+        super().__init__(handle, **kwargs)
+
+class CovidConditionClassifier:
+    def __init__(self, ensemble=False, use_gpu=False) -> None:
+        self.device = '/device:CPU:0' if not use_gpu else '/device:GPU:0'
+        with tf.device(self.device):
+            print("Loading Classification Models......")
+            models = []
+            models0 = tf.keras.models.load_model(f'{MODEL_BASE_PATH}/model0.h5',
+                                        custom_objects={'KerasLayer': KerasLayerWrapper})
+            models.append(models0)
+
+            if ensemble:
+                models1 = tf.keras.models.load_model(f'{MODEL_BASE_PATH}/model1.h5',
+                                                        custom_objects={'KerasLayer': KerasLayerWrapper})
+                models2 = tf.keras.models.load_model(f'{MODEL_BASE_PATH}/model2.h5',
+                                                        custom_objects={'KerasLayer': KerasLayerWrapper})
+                models3 = tf.keras.models.load_model(f'{MODEL_BASE_PATH}/model3.h5',
+                                                        custom_objects={'KerasLayer': KerasLayerWrapper})
+                models4 = tf.keras.models.load_model(f'{MODEL_BASE_PATH}/model4.h5',
+                                                        custom_objects={'KerasLayer': KerasLayerWrapper})
+                models.append(models1)
+                models.append(models2)
+                models.append(models3)
+                models.append(models4)
+
+        print("Successfully loaded Classification Models.....")
+        self.models = models
+
+    def inference(self, img: np.ndarray):
+        with tf.device(self.device):
+            img = tf.convert_to_tensor(img, tf.float32) / 255.0
+            img = tf.image.resize(img, target_size)
+            pred = sum([model.predict(tf.expand_dims(img, axis=0)) for model in self.models]) / len(self.models)
+            pred_label = LABELS[pred.argmax()]
+            confidence = pred[0][pred.argmax()]
+            return {"condition": pred_label, "confidence":confidence.astype(float)}
+
+condition_classifer = CovidConditionClassifier(ensemble=config.ENSEMBLE, use_gpu=config.USE_GPU)
---
+++ backend/inference/covid_detection.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+from mmcv import Config
+from mmdet.apis import init_detector, async_inference_detector, inference_detector
+
+from backend import config
+
+config_path = "models/configs/cascade_rcnn_x101_32x4d_fpn_1x_coco.py"
+model_checkpoint = "models/weights/cascade_rcnn_best_epoch.pth"
+
+class CovidAbnormalityDetector:
+    def __init__(self, use_gpu) -> None:
+        self.use_gpu = use_gpu
+        print("Creating Detection Model...")
+        cfg = Config.fromfile(config_path)
+        print("Loading Detection Model weights from:", model_checkpoint)
+        self.model = init_detector(cfg, model_checkpoint, device='cuda:0')
+        print("Detection Model Loaded...")
+
+    def inference_with_annot_image(self, img:np.ndarray, threshold = 0.45):
+        result = inference_detector(self.model, img)
+        results_filtered = result[0][result[0][:, 4] > threshold]
+        bboxes = results_filtered[:, :4]
+        scores = results_filtered[:, 4]
+        for box in bboxes:
+            img = draw_bbox(img, list(np.int_(box)), "Covid Detected",
+                            (255, 243, 0))
+        return img
+
+    def inference(self, img:np.ndarray, threshold = 0.45):
+        result = inference_detector(self.model, img)
+        results_filtered = result[0][result[0][:, 4] > threshold]
+        return results_filtered
+
+def draw_bbox(image,
+              box,
+              label,
+              color,
+              label_size = 0.5,
+              alpha_box = 0.3,
+              alpha_label = 0.6):
+    
+    overlay_bbox = image.copy()
+    overlay_label = image.copy()
+    output = image.copy()
+
+    text_width, text_height = cv2.getTextSize(label.upper(),
+                                              cv2.FONT_HERSHEY_SIMPLEX, label_size, 1)[0]
+    cv2.rectangle(overlay_bbox, (box[0], box[1]), (box[2], box[3]),
+                  color, -1)
+    cv2.addWeighted(overlay_bbox, alpha_box, output, 1-alpha_box, 0, output)
+    
+    cv2.rectangle(overlay_label, (box[0], box[1]-7-text_height),
+                  (box[0]+text_width+2, box[1]), (0, 0, 0), -1)
+    cv2.addWeighted(overlay_label, alpha_label, output, 1-alpha_label, 0, output)
+    output = cv2.rectangle(output, (box[0], box[1]), (box[2], box[3]),
+                           color, 2)
+    cv2.putText(output, label.upper(), (box[0], box[1]-5),
+            cv2.FONT_HERSHEY_SIMPLEX, label_size, (255, 255, 255), 1, cv2.LINE_AA)
+    return output
+
+abnormality_detector = CovidAbnormalityDetector(use_gpu=config.USE_GPU)
