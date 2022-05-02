81fbc7fa7cdf8902936f53c161fecd3ef342e138
Arun Pandian R <arunrk7.leo@gmail.com>
2022-05-02 11:09:38 +0000

    Fix User ID Reuse

--- backend/endpoints/diagnose.py
+++ backend/endpoints/diagnose.py
@@ -1,9 +1,11 @@
 from io import BytesIO
+from typing import Optional
 import uuid
 
+from pathlib import Path
 import cv2
 import numpy as np
-from fastapi import APIRouter, Depends, File, UploadFile
+from fastapi import APIRouter, Depends, File, Header, UploadFile, Response
 from fastapi.responses import StreamingResponse
 from PIL import Image
 from sqlmodel import Session
@@ -17,27 +19,27 @@ from backend.inference.covid_detection import abnormality_detector
 r = router = APIRouter(prefix="/diagnose")
 
 @r.post("/covid-abnormality")
-def get_abnormalities_detection(file: UploadFile = File(...), session: Session = Depends(get_session)):
-    image, image_id, file_path = save_image(file)
+def get_abnormalities_detection(file: UploadFile = File(...), session: Session = Depends(get_session), image_id: Optional[str] = Header(None)):
+    image, image_id, file_path = save_image(file, image_id)
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-    annotated_image, bbox, confidence, inference_time = abnormality_detector.inference_with_annot_image(image)
+    annotated_image, bbox, inference_time = abnormality_detector.inference_with_annot_image(image)
     annotated_image = Image.fromarray(annotated_image.astype("uint8")).convert("RGB")
     output_image = BytesIO()
     annotated_image.save(output_image, "PNG")
     output_image.seek(0)
-    
-    inf_log = AbnormalityDetectionLog(image_id=image_id, file=str(file_path), inference_time=inference_time, predicted_bbox=bbox, confidence=confidence)
+   
+    inf_log = AbnormalityDetectionLog(image_id=image_id, file=str(file_path), inference_time=inference_time, predicted_bbox=bbox)
     session.add(inf_log)
     session.commit()
     session.refresh(inf_log)
     
-    return StreamingResponse(output_image, media_type="image/png", headers={"image_id":image_id})
+    return StreamingResponse(output_image, media_type="image/png", headers={"x-image-id":image_id})
 
 
 @r.post("/covid-condition")
-def get_condition_classification(file: UploadFile = File(...), session: Session = Depends(get_session)):
-    image, image_id, file_path = save_image(file)
+def get_condition_classification(response: Response, file: UploadFile = File(...), session: Session = Depends(get_session), image_id: Optional[str] = Header(None)):
+    image, image_id, file_path = save_image(file, image_id)
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
     condition, confidence, inf_time = condition_classifer.inference(image)
@@ -47,12 +49,13 @@ def get_condition_classification(file: UploadFile = File(...), session: Session 
     session.commit()
     session.refresh(inf_log)
 
+    response.headers["x-image-id"] = image_id
     return {"condition":condition, "confidence":confidence}
 
 
-def save_image(file: UploadFile):
+def save_image(file: UploadFile, image_id=None):
     image = Image.open(file.file)
-    image_id = str(uuid.uuid4())
-    file_path = config.IMAGE_DB_HOME / image_id+".png"
+    image_id = str(uuid.uuid4()) if not image_id else image_id
+    file_path = config.IMAGE_DB_HOME / Path(image_id+".png")
     image.save(file_path)
     return image, image_id, file_path.name
